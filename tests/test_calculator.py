bdb2be0481d2c67c669cb5f650c2d2d552c12b57
Jonas Neumayer <jonas.neumayer@web.de>
2025-09-02 20:08:43 +0200

    'pyproject.toml'-file added

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,9 +1,10 @@
 import pytest
 
+# from ...unittest_training.src.calculator import DivisionByZeroError, Calculator
 from calculator import DivisionByZeroError, Calculator
 
 
 class TestCalculator:
     @staticmethod
     def test_divide_valid_inputs():
-        assert Calculator.divide(a=2, b=4) == 2 #two integers, two positive numbers, two even numbers
+        assert Calculator.divide(a=2, b=4) == 0.5 #two integers, two positive numbers, two even numbers
