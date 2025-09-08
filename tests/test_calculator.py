6ade35a124a8192bf45d6db99225f5bfb0bb5b49
Jonas Neumayer <jonas.neumayer@web.de>
2025-09-08 15:10:16 +0200

    'calculator.py' and 'test_calculator.py' refactored

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,7 +1,6 @@
 import pytest
 
-# from ...unittest_training.src.calculator import DivisionByZeroError, Calculator
-from calculator import DivisionError, Calculator
+from calculator import DivisionByZeroError, InvalidInputError, Calculator
 
 
 class TestCalculator:
@@ -9,5 +8,18 @@ class TestCalculator:
     def test_divide_valid_inputs():
         assert Calculator.divide(a=2, b=4) == 0.5 #two positive numbers
         assert Calculator.divide(a=2, b=-5) == -0.4 #one positive and one negative number
-        assert Calculator.divide(a=-2.5, b=-5) == 0.5 #two negative numbers
-        assert Calculator.divide(a=0, b=0.01) == 0 #numerator is zero
+        assert Calculator.divide(a=-2.5, b=-5) == 0.5 #two negative numbers (one is float)
+        assert Calculator.divide(a=0, b=0.01) == 0 #numerator is zero (one is float)
+    
+    @staticmethod
+    def test_divide_denominator_zero():
+        with pytest.raises(DivisionByZeroError):
+            Calculator.divide(a=2.4, b=0)
+    
+    @staticmethod
+    def test_divide_invalid_input_types():
+        with pytest.raises(InvalidInputError):
+            Calculator.divide(a=2.4, b="Hello") #one valid input, one invalid input
+        
+        with pytest.raises(InvalidInputError):
+            Calculator.divide(a="Hello", b="Hello") #two valid inputs
