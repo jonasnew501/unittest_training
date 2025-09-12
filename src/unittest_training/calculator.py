5040ffd898b058ed9ea6e011dd9f7149709c548c
Jonas Neumayer <jonas.neumayer@web.de>
2025-09-12 16:19:20 +0200

    Auto-formatted code by black over the whole project

--- src/unittest_training/calculator.py
+++ src/unittest_training/calculator.py
@@ -2,24 +2,27 @@ from typing import Union
 
 
 class DivisionByZeroError(Exception):
-    '''
+    """
     A custom domain-specific Exception
     for dividing by zero.
-    '''
+    """
+
     pass
 
+
 class InvalidInputError(Exception):
-    '''
+    """
     A custom domain-specific Exception
     for providing invalid inputs to functions.
-    '''
+    """
+
     pass
 
 
 class Calculator:
     @staticmethod
     def divide(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
-        '''
+        """
         Divide 'a' by 'b'.
 
         Args:
@@ -28,22 +31,22 @@ class Calculator:
 
         Returns:
             float: The result of the division.
-        
+
         Raises:
             DivisionByZeroError: If denominator is zero.
             InvalidInputError: If a and/or b are not int/float.
-        '''
+        """
         if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
             raise InvalidInputError("Operands must be int or float.")
-        
+
         if b == 0:
             raise DivisionByZeroError("Denominator cannot be zero.")
-    
+
         # #New feature: Integer division (return an int when both arguments are of type int)
         # if isinstance(a, int) and isinstance(b, int):
         #     return a // b #returns an int
-        
-        return a/b
+
+        return a / b
 
 
 # print(Calculator.divide(4,2))
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,6 +1,10 @@
 import pytest
 
-from unittest_training.calculator import DivisionByZeroError, InvalidInputError, Calculator
+from unittest_training.calculator import (
+    DivisionByZeroError,
+    InvalidInputError,
+    Calculator,
+)
 
 
 class TestCalculator:
@@ -11,55 +15,53 @@ class TestCalculator:
     #     assert Calculator.divide(a=2, b=-5) == -0.4 #one positive and one negative number
     #     assert Calculator.divide(a=-2.5, b=-5) == 0.5 #two negative numbers (one is float)
     #     assert Calculator.divide(a=0, b=0.01) == 0 #numerator is zero (denominator is float)
-    
+
     @staticmethod
-    #The version of this function with pytests parameterization
+    # The version of this function with pytests parameterization
     @pytest.mark.parametrize(
         "a, b, expected",
         [
-            (2, 4, 0.5), #two positive numbers
-            (2, -5, -0.4), #one positive and one negative number
-            (-2.5, -5, 0.5), #two negative numbers (one is float)
-            (0, 0.01, 0), #numerator is zero (denominator is float)
-        ]
+            (2, 4, 0.5),  # two positive numbers
+            (2, -5, -0.4),  # one positive and one negative number
+            (-2.5, -5, 0.5),  # two negative numbers (one is float)
+            (0, 0.01, 0),  # numerator is zero (denominator is float)
+        ],
     )
     def test_divide_valid_inputs_correct_result(a, b, expected):
         assert Calculator.divide(a, b) == expected
-    
-    
+
     @staticmethod
-    #The version of this function with pytests parameterization
+    # The version of this function with pytests parameterization
     @pytest.mark.parametrize(
         "a, b",
         [
-            (2, 4), #two positive numbers
-            (2, -5), #one positive and one negative number
-            (-2.5, -5), #two negative numbers (one is float)
-            (0, 0.01), #numerator is zero (denominator is float)
-        ]
+            (2, 4),  # two positive numbers
+            (2, -5),  # one positive and one negative number
+            (-2.5, -5),  # two negative numbers (one is float)
+            (0, 0.01),  # numerator is zero (denominator is float)
+        ],
     )
     def test_divide_valid_inputs_correct_datatype(a, b):
         assert isinstance(Calculator.divide(a, b), float)
 
-    
     @staticmethod
     def test_divide_denominator_zero():
         with pytest.raises(DivisionByZeroError):
-            Calculator.divide(a=2.4, b=0) #numerator positive, denominator zero
+            Calculator.divide(a=2.4, b=0)  # numerator positive, denominator zero
 
         with pytest.raises(DivisionByZeroError):
-            Calculator.divide(a=-2.4, b=0) #numerator negative, denominator zero
+            Calculator.divide(a=-2.4, b=0)  # numerator negative, denominator zero
 
         with pytest.raises(DivisionByZeroError):
-            Calculator.divide(a=0, b=0) #numerator zero, denominator zero
-        
+            Calculator.divide(a=0, b=0)  # numerator zero, denominator zero
+
         with pytest.raises(DivisionByZeroError):
-            Calculator.divide(a=0, b=-0) #numerator zero, denominator negative zero
-    
+            Calculator.divide(a=0, b=-0)  # numerator zero, denominator negative zero
+
     @staticmethod
     def test_divide_invalid_input_types():
         with pytest.raises(InvalidInputError):
-            Calculator.divide(a=2.4, b="Hello") #one valid input, one invalid input
-        
+            Calculator.divide(a=2.4, b="Hello")  # one valid input, one invalid input
+
         with pytest.raises(InvalidInputError):
-            Calculator.divide(a="Hello", b="Hello") #two valid inputs
+            Calculator.divide(a="Hello", b="Hello")  # two valid inputs
