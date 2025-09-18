cd9c41e614ae76f14866ba687c7aa6db0e26df4d
Jonas Neumayer <jonas.neumayer@web.de>
2025-09-18 20:09:24 +0200

    Auto-formatting conducted for 'calculator_mocking' and 'test_calculator_mocking'

--- src/unittest_training/calculator_mocking.py
+++ src/unittest_training/calculator_mocking.py
@@ -1,5 +1,6 @@
 from typing import Union
 
+
 class InvalidInputError(Exception):
     """
     A custom domain-specific Exception
@@ -14,22 +15,20 @@ class CalculatorMocking:
     def add(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
         if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
             raise InvalidInputError("Operands must be int or float.")
-        
+
         return a + b
 
     @staticmethod
     def multiply(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
         if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
             raise InvalidInputError("Operands must be int or float.")
-        
+
         result = 0
-        for i in range(1, b+1):
+        for i in range(1, b + 1):
             result = CalculatorMocking.add(result, a)
             print(f"Multiply-iteration: {i}")
-        
+
         return result
 
 
 # print(multiply(3, 4))
-
-
--- tests/test_calculator_mocking.py
+++ tests/test_calculator_mocking.py
@@ -1,27 +1,31 @@
 import pytest
 
-from unittest_training.calculator_mocking import(
+from unittest_training.calculator_mocking import (
     InvalidInputError,
     CalculatorMocking,
 )
+
 
 class TestCalculatorMocking:
     @staticmethod
     @pytest.mark.parametrize(
         "a, b",
         [
-            (4.387, "Hello"), #a is valid, b is invalid
-            ("Hello", 7), #a is invalid, b is valid
-            ("Hello", "Bye"), #a is invalid, b is invalid
-        ]
+            (4.387, "Hello"),  # a is valid, b is invalid
+            ("Hello", 7),  # a is invalid, b is valid
+            ("Hello", "Bye"),  # a is invalid, b is invalid
+        ],
     )
     def test_multiply_invalid_inputs(a, b):
         with pytest.raises(InvalidInputError, match="Operands must be int or float."):
             CalculatorMocking.multiply(a=a, b=b)
-    
+
     @staticmethod
     def test_multiply_valid_inputs_with_mock(mocker):
-        fake_add = mocker.patch("unittest_training.calculator_mocking.CalculatorMocking.add", return_value=10)
+        fake_add = mocker.patch(
+            "unittest_training.calculator_mocking.CalculatorMocking.add",
+            return_value=10,
+        )
 
         result = CalculatorMocking.multiply(3, 4)
 
@@ -42,35 +46,35 @@ class TestCalculatorMocking:
         #'assert_called_with' checks only for the last-call of the patched object
         fake_add.assert_called_with(10, 3)
 
-        #for inspecting the whole call-history of the patched object
+        # for inspecting the whole call-history of the patched object
         print(fake_add.call_args_list)
 
-        #------------
-        #with 'side_effect's, the mocked function can be made to behave differently
-        #each time it is called (when an iterable is assigned as the side_effect)
+        # ------------
+        # with 'side_effect's, the mocked function can be made to behave differently
+        # each time it is called (when an iterable is assigned as the side_effect)
         fake_add.side_effect = [1, 2, 3]
 
         result = CalculatorMocking.multiply(3, 3)
 
-        assert result == 3 #3, because the first call of 'add' returns 1, the second call returns 2,
-                           #and the third call returns 3
-        
-        #-----
+        assert (
+            result == 3
+        )  # 3, because the first call of 'add' returns 1, the second call returns 2,
+        # and the third call returns 3
 
-        #with 'side_effect's, the mocked function can also be made to return an
-        #Exception. This is how failures in dependencies (here: the 'add'-function)
-        #can be simulated.
+        # -----
+
+        # with 'side_effect's, the mocked function can also be made to return an
+        # Exception. This is how failures in dependencies (here: the 'add'-function)
+        # can be simulated.
         fake_add.side_effect = ValueError("Error!")
         try:
             CalculatorMocking.multiply(3, 3)
         except:
             pass
-        #------------
+        # ------------
 
-        #Using 'mocker.Mock()' directly
+        # Using 'mocker.Mock()' directly
         fake_add = mocker.Mock(return_value=42)
 
         assert fake_add(1, 2, 3) == 42
         fake_add.assert_called_once_with(1, 2, 3)
-
-        
